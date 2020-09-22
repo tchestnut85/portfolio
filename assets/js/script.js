2fc9beaea34827d9b2436c4d0a20b3f87e8b0427
Tom Chestnut <tchestnut85@gmail.com>
2020-09-22 17:14:12 -0400

    updated nav bar with mobile dropdown menu

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// var instance = M.Dropdown.getInstance(elem);
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.dropdown-button');
+//     var instances = M.Dropdown.init(elems, options);
+// });
+
+  $('.dropdown-button').dropdown();
